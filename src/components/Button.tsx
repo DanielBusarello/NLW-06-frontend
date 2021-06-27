af48b7edef7a38a05e19a969d342f1bdd0f89518
Daniel Busarello <62729531+DanielBusarello@users.noreply.github.com>
2021-06-27 19:07:09 -0300

    Página inicial e autenticação

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,32 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from 'react'
 
-type ButtonProps = {
-    text?: string;
-}
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
-    const [counter, setCounter] = useState(0);
-    
-    function increment(){
-        setCounter(counter + 1);
-        console.log(counter);
-    }
-
-    return (
-        <div>
-            <button onClick={increment}>{props.text || "Default"}</button>
-        </div>
-    )
+  return (
+    <button className="button" {...props} />
+  )
 }
+
+// import { useState } from "react";
+
+// type ButtonProps = {
+//     text?: string;
+// }
+
+// export function Button(props: ButtonProps) {
+//     const [counter, setCounter] = useState(0);
+    
+//     function increment(){
+//         setCounter(counter + 1);
+//         console.log(counter);
+//     }
+
+//     return (
+//         <div>
+//             <button onClick={increment}>{props.text || "Default"}</button>
+//         </div>
+//     )
+// }
